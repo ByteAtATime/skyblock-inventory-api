4b99a7bfe4b7f4cd95259dfbb068855aa1e84495
ByteAtATime <byteatatime@proton.me>
2024-12-14 14:56:52 -0800

    feat: inventory data

---
+++ src/pages/api/[player]/[profile]/inventory.ts
@@ -0,0 +1,58 @@
+import type {APIRoute} from "astro";
+import {z} from "zod";
+import {parse} from "prismarine-nbt";
+
+const playerDataSchema = z.object({
+    profiles: z.array(z.object({
+        profile_id: z.string(),
+        members: z.record(z.string(), z.object({
+            inventory: z.object({
+                inv_contents: z.object({
+                    data: z.string(),
+                }),
+            }).optional(),
+        })),
+    })),
+});
+
+export const GET: APIRoute = async ({params}) => {
+    const { player, profile: profileUuid } = params;
+
+    if (!player) {
+        return new Response(JSON.stringify({ success: false, error: "Missing player UUID" }), { status: 400 });
+    }
+
+    if (!profileUuid) {
+        return new Response(JSON.stringify({ success: false, error: "Missing profile UUID" }), { status: 400 });
+    }
+
+    const res = await fetch(`https://api.hypixel.net/v2/skyblock/profiles?uuid=${player}`, {
+        headers: {
+            "API-Key": import.meta.env.HYPIXEL_API_KEY
+        }
+    })
+
+    const rawData = await res.json();
+    const { data, error } = playerDataSchema.safeParse(rawData);
+
+    if (error || !data) {
+        return new Response(JSON.stringify({ success: false, error }), { status: 500 });
+    }
+
+    const profile = data.profiles.find(p => p.profile_id === profileUuid);
+    const playerProfile = profile?.members[player.replaceAll("-", "")];
+
+    if (!profile || !playerProfile) {
+        return new Response(JSON.stringify({ success: false, error: `Player does not have profile ${profileUuid}` }), { status: 400 });
+    }
+
+    const inventory = playerProfile.inventory?.inv_contents?.data;
+
+    if (!inventory) {
+        return new Response(JSON.stringify({ success: false, error: "Cannot find inventory data; player may not have enabled API" }), { status: 400 });
+    }
+
+    const decodedInventory = await parse(new Buffer(inventory, "base64"));
+
+    return new Response(JSON.stringify(decodedInventory));
+}
